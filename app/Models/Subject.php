ad08a80d86c13cde1928ec4e1c29a778c8b92e24
Abrorcr7 <47205510+Abrorcr7@users.noreply.github.com>
2019-05-05 21:13:03 +0500

    update subject model

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     protected $table = 'subject';
-    protected $fillable = [
-        'fs',
-    ];
+    protected $fillable = ['fs'];
 }
